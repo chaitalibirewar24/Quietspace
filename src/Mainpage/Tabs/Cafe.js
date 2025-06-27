15d6f4c9e2cf7330251abb288e2cbd8dbbb1c8bf
shalaka149 <shalakakelkar25@gmail.com>
2025-06-27 23:45:53 +0530

    last changes to branch

--- src/Mainpage/Tabs/Cafe.js
+++ src/Mainpage/Tabs/Cafe.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import cafes from '../../../backend/places';
+//import cafes from '../../../backend/places';
 
 const Cafes = () => {
   return (
     <div style={{ padding: '2rem' }}>
       <h2>Cafes Page</h2>
     </div>
-    <div>
-      <img src=""
-    </div>
-
   );
 };
 
